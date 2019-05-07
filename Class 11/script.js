2c120d23dcfb6c45074edf3597961758a4787d22
Chase Nguyen <chasebaonguyen@gmail.com>
2019-05-06 20:02:20 -0700

    adding transition effects and test
    transition effects and test

--- Class 11/script.js
+++ Class 11/script.js
@@ -8,25 +8,19 @@ let toggleNav = function() {
 
     if (toggleNavStatus === false) {
         getSidebarUl.style.visibility = "visible";
-        getSidebar.style.width  = "272px";
+        getSidebar.style.width  = "180px";
         getSidebarTitle.style.opacity = "1";
 
-        let arrayLength = getSidebarLinks.length;
-        for (let i = 0; i < arrayLength.length; i++) {
-            getSidebarLinks[i].style.opacity = "1";
-        }
+        
 
         toggleNavStatus = true;
     }
 
     else if (toggleNavStatus === true) {
-        getSidebar.style.width  = "50px";
+        getSidebar.style.width  = "0px";
         getSidebarTitle.style.opacity = "0";
 
-        let arrayLength = getSidebarLinks.length;
-        for (let i = 0; i < arrayLength.length; i++) {
-            getSidebarLinks[i].style.opacity = "0";
-        }
+        
 
         getSidebarUl.style.visibility = "hidden";
 
